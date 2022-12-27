4ee8fc447662b1211e059587fb6b6b4101d6704a
kuangxianfeng <747779672@qq.com>
2022-12-27 21:25:16 +0800

    fix: 提交剩余部分

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -1,8 +1,7 @@
 import type { MenuProps } from 'antd'
 import type { RouteObject } from 'react-router-dom'
 /** antd 菜单栏类型 */
-export type AntdSiderListProps = Required<MenuProps>["items"]
-
+export type AntdSiderListProps = Exclude<Required<MenuProps>["items"][number], null>[]
 /**
  * React 路由数组
  */
--- src/utils/diff-authorization.ts
+++ src/utils/diff-authorization.ts
@@ -1,47 +1,54 @@
-import { AntdSiderListProps, RoutesListTypes } from "@/common/types"
+import {  RoutesListTypes } from "@/common/types"
 import { AllMenuList, AllRouteList } from "@/config/all-authorization-list"
 
-export type A = {
-    children?: A[]
+export type TreeDataTypes = {
+    children?: TreeDataTypes[]
+    key?: string
+    path?: string
 }
 
-export function flatteningTreeWrap<T>(treeWrap: T) {
-    const array: T = []
+export function flatteningTreeWrap<T extends TreeDataTypes[]>(treeWrap: T) {
+    const array = [] as unknown as T
     const flatteningTree = (tree: T) => {
         tree.forEach(item => {
             array.push(item)
-            item.children && item.children.length > 0 && flatteningTree(item.children)
+            item.children && item.children.length > 0 && flatteningTree(item.children as unknown as T)
         })
     }
     flatteningTree(treeWrap)
     return array
 }
 
-export function findChildrenDelete(treeData, fn) {
+export function findChildrenDelete<T extends TreeDataTypes[]>(treeData: T, fn: (item: TreeDataTypes) => boolean) {
     treeData.map((item, index) => {
         fn(item) && treeData.splice(index, 1)
         item.children && findChildrenDelete(item.children, fn)
     })
     return treeData
 }
-export function treeFindPath(tree, func, path = []) {
-    if (!tree) return []
-    for (const data of tree) {
-        path.push(data.key)
-        if (func(data)) return path
-        if (data.children) {
-            const findChildren = treeFindPath(data.children, func, path)
-            if (findChildren.length > 0) return findChildren
+export function treeFindPath<T extends TreeDataTypes[], K = TreeDataTypes>(treeData: T, func: (item: K) => boolean): string[] {
+    if (!treeData) return []
+    const arr: string[] = []
+    const treeFindPathOuter = (tree: T) => {
+        for (const data of tree) {
+            arr.push(data.key!)
+            if (func(data as unknown as K)) return arr
+            if (data.children) {
+                const findChildren = treeFindPath(data.children, func)
+                if (findChildren.length > 0) return findChildren
+            }
+            arr.pop()
         }
-        path.pop()
     }
+    treeFindPathOuter(treeData)
     return []
 }
-export function filterAuthorizationSiderList(treeData: AntdSiderListProps) {
+export function filterAuthorizationSiderList<T extends TreeDataTypes[]>(treeData: T) {
     const flatteningTree = flatteningTreeWrap(treeData)
-    return findChildrenDelete(AllMenuList, data => !flatteningTree.find(item => item.path === data.key))
+    return findChildrenDelete(AllMenuList as any, data => !flatteningTree.find(item => item.path === data.key))
 }
 export const filterAuthorizationRoutesList = (treeData: RoutesListTypes) => {
     const flatteningTree = flatteningTreeWrap(treeData)
     return findChildrenDelete(AllRouteList, data => !flatteningTree.find(item => item.path === data.path))
 }
+
